b6c56ae365487104dd02c3cbf64c4a55d7423b95
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-02-24 16:29:21 +0100

    Update osmosis_validators.py

--- osmosis_validators.py
+++ osmosis_validators.py
@@ -123,7 +123,7 @@ def get_query_results(token):
     
     return data
  
-set_background('./sincerely-media-cuEpo721ACY-unsplash.jpg')
+set_background('sincerely-media-cuEpo721ACY-unsplash.jpg')
 
 st.header("") 
 
