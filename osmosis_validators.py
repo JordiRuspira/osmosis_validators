7fa96ef617a9fcc301dd39a2cd6d318ecdb08dec
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-02-24 22:14:27 +0100

    Update osmosis_validators.py

--- osmosis_validators.py
+++ osmosis_validators.py
@@ -122,25 +122,7 @@ def get_query_results(token):
         return get_query_results(token)
     
     return data
- 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    
-set_background('./images/sincerely-media-cuEpo721ACY-unsplash.jpg')
+  
 
 st.header("") 
 
